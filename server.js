0e91d40af1b65b37af7d133813e4bf95349d8c1d
Zeke <zeke@shopprivato.com>
2014-08-16 19:15:01 -0400

    changed to more up to date NPM module for icon, used static-icon, changed to serve-icon

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -24,7 +24,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.set('port' || 3000);
 
-app.use(favicon());
+app.use(favicon(__dirname + '/public/favicon.png'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
